214e12e88ecf655c5aaeb3cc8daa91c865dd766d
Ondicho <ondicho53@gmail.com>
2020-09-14 08:04:21 +0300

    adds properties and getters to Squad object and their accompanying tests

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -2,4 +2,28 @@ public class Squad {
     private String name;
     private String cause;
     private int maxSize;
+    private Hero squadList;
+
+    public Squad(String name, String cause, int maxSize, Hero squadList){
+        name=name;
+        cause=cause;
+        maxSize=maxSize;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public Hero getSquadList() {
+        return squadList;
+    }
+
+    public int getMaxSize() {
+        return maxSize;
+    }
 }
